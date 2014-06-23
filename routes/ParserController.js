a61c0a635f51ae9e8892d6cf15e37ee888c9f0a4
Hugo ter Doest <hwl.ter.doest@gmail.com>
2014-06-23 23:21:15 +0200

    Added presentation of Earley parse results

--- routes/ParserController.js
+++ routes/ParserController.js
@@ -35,24 +35,38 @@ exports.input_sentence = function(req, res) {
 exports.parse_sentence = function(req, res) {
   var chart_CYK, chart_Earley;
   var sentence;
-  var start, end, accepted;
-  var CYK_time, Earley_time;
+  var start, end, accepted_CYK, accepted_Earley;
+  var time_CYK, time_Earley;
+
+  sentence = CYK.tokenize_sentence(req.param('input_sentence'));
+  var N = sentence.length;
   
   // CYK
-  sentence = CYK.tokenize_sentence(req.param('input_sentence'));
-  start = new Date();
+  start = new Date().getTime();
   chart_CYK = CYK.CYK_Chart_Parser(sentence);
-  end = new Date();
-  CYK_time = end - start;
+  end = new Date().getTime();
+  time_CYK = end - start;
   console.log(chart_CYK);
-
+  accepted_CYK = chart_CYK[N - 1][0] ? (chart_CYK[N - 1][0].indexOf(Grammar.start_symbol()) !== -1) : false;
+  
   // Earley
-  start = new Date();
+  start = new Date().getTime();
   chart_Earley = EarleyChartParser.earley_parse(sentence);
-  end = new Date();
-  Earley_time = end - start;
+  end = new Date().getTime();
+  console.log(end);
+  time_Earley = end - start;
   console.log(chart_Earley);
   
-  accepted = chart_CYK[sentence.length - 1][0] ? (chart_CYK[sentence.length - 1][0].indexOf(Grammar.start_symbol()) !== -1) : false;
-  res.render('parse_result', {chart: chart_CYK, N: sentence.length, sentence: sentence, parsing_time: CYK_time, in_language: accepted});
+  accepted_Earley = false;
+  var keys_of_final_state = Object.keys(chart_Earley[N]);
+  keys_of_final_state.forEach(function(key) {
+    var item = chart_Earley[N][key];
+    if ((item.rule.lhs === Grammar.start_symbol()) && (item.rule.rhs.length === item.dot)) {
+      accepted_Earley = true;
+    }
+  });
+
+  res.render('parse_result', {chart_CYK: chart_CYK, parsing_time_CYK: time_CYK, in_language_CYK: accepted_CYK,
+                              chart_Earley: chart_Earley, parsing_time_Earley: time_Earley, in_language_Earley: accepted_Earley,
+                              N: N, sentence: sentence });
 };
