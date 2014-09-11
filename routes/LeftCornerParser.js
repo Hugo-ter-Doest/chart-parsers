7467785690ab0456ae3b41679f7827f5d723e6eb
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2014-09-11 22:09:06 +0200

    Skeleton for the left-corner parser.

--- routes/LeftCornerParser.js
+++ routes/LeftCornerParser.js
@@ -16,39 +16,53 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var ChartParser = require('./ChartParser');
-var Item = require('./Item');
+var Chart = require('./Chart');
+var Item = require('Item');
 
-// inheritance according to http://book.mixu.net/node/ch6.html:
+// Constructur for the left-corner parser
 function LeftCornerChartParser(grammar) {
-  grammar.compute_lc_relation();
-  ChartParser.apply(this, Array.prototype.slice.call(arguments));
+  this.grammar = grammar;
 }
 
-LeftCornerChartParser.prototype = new ChartParser();
+LeftCornerChartParser.prototype.initialise = function(N) {
+  // Initialise chart
+  this.chart = new Chart(N);
+  // Add items for rules that have the start symbol as left-hand-side
+  
+};
 
-// Introduces new items for the next nonterminal to be recognised
-// Improved left-corner based on:
-// http://www.umiacs.umd.edu/~resnik/ling723_fa2006/assignments/7/iwpt2000-rev3.pdf
-LeftCornerChartParser.prototype.predictor = function(item, j) {
-  console.log("Predictor: " + item.id + j);
-  // remember the size of the set at position j 
-  var nr_items = this.chart.nr_items_to(j);
-  console.log("Nummber of items before: " + nr_items);
-  // B is the next nonterminal to be recognised
-  var B = item.data.rule.rhs[item.data.dot];
-  // Get all rules with lc B
-  var rules_lc_B = this.grammar.rules_with_lc(B);
-  // for each rule with LHS B create an item
-  var that = this;
-  rules_lc_B.forEach(function(rule) {
-      var newitem = new Item(rule, 0, j);
-      that.chart.add_item(j, j, newitem);
-      console.log("Predictor: added item " + newitem.id  + " to state " + j);
-  });
-  console.log("Nummber of items after: " + this.chart.nr_items_to(j));
-  // Return number of items added
-  return(this.chart.nr_items_to(j) - nr_items);
+LeftCornerChartParser.prototype.completer = function() {
+  
+};
+
+LeftCornerChartParser.prototype.scanner = function() {
+  
+};
+
+LeftCornerChartParser.prototype.lc_predictor = function() {
+  
+};
+
+LeftCornerChartParser.prototype.lc_predictor_scanner = function() {
+  
+};
+
+LeftCornerChartParser.prototype.parse = function(tagged_sentence) {
+  
+  N = tagged_sentence.length;
+  this.initialise(n);
+  
+  var i;
+  for (i = 0; i <= N; i++) {
+    var items_added = 0;
+    do {
+      items_added += scanner(i);
+      items_added += completer(i);
+      items_added += lc_predictor(i);
+      items_added += lc_predictor_scanner(i);
+    } while (items_added);
+  }
+  return this.chart;
 };
 
 module.exports = LeftCornerChartParser;
