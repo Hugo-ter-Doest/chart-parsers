8b517d4dd25694904cf6842bd86b95eafe864c5c
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2014-10-04 20:58:12 +0200

    Added unit tests for CYKParser and Chart.

---
+++ spec/CYKParser_spec.js
@@ -0,0 +1,85 @@
+/*
+    Unit test for CYKParser.js
+    Copyright (C) 2014 Hugo W.L. ter Doest
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var assert = require('assert');
+var fs = require('fs');
+
+var GrammarParser = require('../lib/GrammarParser');
+var CYK_ChartParser = require('../lib/CYKParser');
+var Item = require('../lib/Item');
+
+// Grammar (in ../data/test_grammar_for_CYK.txt)
+//S  -> NP VP
+//NP -> DET N
+//NP -> NP PP
+//PP -> P NP
+//VP -> V NP
+//VP -> VP PP
+
+// Lexicon
+//DET -> the
+//NP -> I
+//N -> man
+//N -> telescope
+//P -> with
+//V -> saw
+//N -> cat
+//N -> dog
+//N -> pig
+//N -> hill
+//N -> park
+//N -> roof
+//P -> from
+//P -> on
+//P -> in
+
+  describe('CYK Parser', function() {
+    var grammar_text;
+    
+    it('should read a text file', function(done) {
+      fs.readFile('data/test_grammar_for_CYK.txt', 'utf8', function (error, text) {
+        expect(text).toBeDefined();
+        grammar_text = text;
+        console.log(grammar_text+"!")
+        done();
+      });
+    });
+
+    var grammar;
+    it('should parse the text file with the grammar', function() {
+      grammar = GrammarParser.parse(grammar_text);
+    });
+    
+    it('should parse a sentence', function() {
+      var tagged_sentence = [['I', 'NP'],
+                             ['saw', 'V'],
+                             ['the', 'DET'],
+                             ['man', 'N'],
+                             ['with', 'P'],
+                             ['the', 'DET'],
+                             ['telescope', 'N']];
+      var N = tagged_sentence.length;
+      var parser = new CYK_ChartParser(grammar);
+      var chart = parser.parse(tagged_sentence);
+      var parses = chart.full_parse_items(grammar.get_start_symbol());
+      expect(parses.length).toEqual(2);
+      var expected_item = new Item({'lhs': 'S', 'rhs': ['NP', 'VP']}, 2, 0, 7);
+      expect(parses[0].id, expected_item.id).toEqual(expected_item.id);
+      expect(parses[1].id, expected_item.id).toEqual(expected_item.id);
+    });
+});
---
+++ spec/Chart_spec.js
@@ -0,0 +1,70 @@
+/*
+    Unit test for Chart.js using Jasmine
+    Copyright (C) 2014 Hugo W.L. ter Doest
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var typeOf = require('typeof');
+
+var Chart = require('../lib/Chart.js');
+var Item = require('../lib/Item.js');
+
+describe('Chart', function() {
+  var chart;
+  var size = 6;
+  
+  it('should create an empty chart', function() {
+    chart = new Chart(size);
+    expect(chart.N).toEqual(size);
+    expect(chart.outgoing_edges.length).toEqual(size + 1);
+    expect(chart.incoming_edges.length).toEqual(size + 1);
+    expect(typeOf(chart.incoming_edges)).toEqual('array');
+    expect(typeOf(chart.outgoing_edges)).toEqual('array');
+  });
+
+  var i1 = new Item({'lhs': 'S', 'rhs': ['NP', 'VP']}, 0, 0, 1);
+  var i2 = new Item({'lhs': 'S', 'rhs': ['NP', 'VP']}, 1, 1, 2);
+  var i3 = new Item({'lhs': 'S', 'rhs': ['NP', 'VP']}, 2, 2, 3);
+  var i4 = new Item({'lhs': 'S', 'rhs': ['NP', 'VP']}, 2, 0, 6);
+  i4.set_children([i1, i2, i3]);
+
+  it('should add items to the chart', function() {
+    chart.add_item(i1);
+    chart.add_item(i2);
+    chart.add_item(i3);
+    expect(chart.get_items_from_to(0, 1)).toEqual([i1]);
+    expect(chart.get_items_from_to(1, 2)).toEqual([i2]);
+    expect(chart.get_items_from_to(2, 3)).toEqual([i3]);
+    expect(chart.get_items_from(0)).toEqual([i1]);
+    expect(chart.get_items_from(1)).toEqual([i2]);
+    expect(chart.get_items_from(2)).toEqual([i3]);
+    expect(chart.get_items_to(1)).toEqual([i1]);
+    expect(chart.nr_items_to(1)).toEqual(1);
+    expect(chart.get_items_to(2)).toEqual([i2]);
+    expect(chart.nr_items_to(2)).toEqual(1);
+    expect(chart.get_items_to(3)).toEqual([i3]);
+    expect(chart.nr_items_to(3)).toEqual(1);
+    expect(chart.get_complete_items_from_to(2, 3)).toEqual([i3]);
+    expect(chart.full_parse_items()).toEqual([]);
+    chart.add_item(i4);
+    expect(chart.get_complete_items_from_to(0, 6)).toEqual([i4]);
+    expect(chart.full_parse_items('S')).toEqual([i4]);
+    expect(chart.nr_of_items()).toEqual(4);
+  });
+
+  it('should generate parse trees from the chart', function() {
+    expect(chart.parse_trees('S')).toEqual(["S(S(NP,VP),S(NP,VP),S(NP,VP))"]);
+  });
+});
