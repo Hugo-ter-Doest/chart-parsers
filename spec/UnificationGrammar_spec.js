9e8dd1e19dfb0f905b0bb3f613a6edd93e0b80a6
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2015-04-23 20:46:26 +0200

    Working on unification grammar parsing test

---
+++ spec/UnificationGrammar_spec.js
@@ -0,0 +1,46 @@
+/*
+    Unification grammar integration test: tests combination of TypeLattice, 
+    LexiconParser, and ChartParser
+    Copyright (C) 2015 Hugo W.L. ter Doest
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var base = '/home/hugo/Workspace/chart_parser/data/';
+
+var lexicon_file = base + 'UG_lexicon.txt';
+var type_lattice_file = base + 'UG_lattice.txt';
+var grammar_file = base + 'UG_grammar.txt';
+var sentences_file = base + 'UG_sentences.txt';
+
+describe('Unification grammar chain', function() {
+
+  it('should correctly read a type lattice', function() {
+    
+  });
+  
+  it('should correctly read a lexicon', function() {
+    
+  });
+  
+  it('should correctly read a unification grammar', function() {
+    
+  });
+  
+  it('should correctly parse a set of sentences using unification grammar', function() {
+    
+  });
+  
+});
+  
